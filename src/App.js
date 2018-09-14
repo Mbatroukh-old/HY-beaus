023f99b20bf5c368126bf4bf980d57f1a21e375e
Mohammad Batroukh <mbatroukh@gmail.com>
2018-09-13 23:00:41 -0400

    Successfull connection to API

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  componentDidMount(){
+    // let url = "//lcboapi.com/products";
+    // let headers = { "Content-Type": "application/json" };
+    // let token = "";
+    let headers = {"Authorization": "Token MDo1MTc5ZTExOC1iN2M2LTExZTgtYjdmMS03N2FjMmRiMmZhZWE6c293aWFiWkFtMGtzTVFRa25pU1Z4WWZheTdCSkF1YXZ6ZG02"}
+    return (
+      fetch("//lcboapi.com/products?where=is_seasonal", { headers })
+        // function getBeer() {
+        // return fetch({`${url}$?where=is_seasonal`})
+        .then(response => response.json())
+        .then(responseJson => {
+          return responseJson.result;
+        })
+        .catch(error => {
+          console.error(error);
+        })
+    );
+    // }
+    // getBeer();
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +32,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started,asdasdasd edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
